77609e0722a347f8b44e5f14cdc05d703b41434e
Arvik143Prime <arvik3cr@gmail.com>
2026-01-12 00:36:08 +0530

    Fix: package-safe import in models

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from datetime import datetime
-from database import Base
+from backend.database import Base
 
 
 class Article(Base):
